72e2b3d5042fd6e2b73fd6f2b903ae09727b3286
dayanasofiaosorioramos618-maker <dayanasofiaosorioramos618@gmail.com>
2026-07-31 12:54:41 -0500

    pruebas
    prueba de despliegue

--- sgeiuan/settings.py
+++ sgeiuan/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4a*jvbxbjrzr9xmmj+0n2=0ix!2)j0&*=t3&k0464x6mivdexl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",".onrender.com","localhost","127.0.0.1"]
+
 
 
 # Application definition
