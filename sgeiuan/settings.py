70e452443d9d51839c12dd882b38dfeab935a0fc
dayanasofiaosorioramos618-maker <dayanasofiaosorioramos618@gmail.com>
2026-08-01 11:57:03 -0500

    Update settings.py
    linea 28 duplicada

--- sgeiuan/settings.py
+++ sgeiuan/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = 'django-insecure-4a*jvbxbjrzr9xmmj+0n2=0ix!2)j0&*=t3&k0464x6mivdexl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS = ["*",".onrender.com","localhost","127.0.0.1"]
 
 
