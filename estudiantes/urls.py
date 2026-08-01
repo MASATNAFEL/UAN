5574c77ba87d55d5f292ba6f1ceeb715d67948f0
dayanasofiaosorioramos618-maker <dayanasofiaosorioramos618@gmail.com>
2026-08-01 11:55:16 -0500

    segunda prueba
    modificacion de vista

--- estudiantes/urls.py
+++ estudiantes/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("estudiantes/", views.index, name="index"),
     path("info/<str:cedula_ciudadania>/", views.informacion, name="informacion"),
     path("info/<str:cedula_ciudadania>/edit", views.editar, name="editar"),
     path("info/<str:cedula_ciudadania>/borrar", views.borrar, name="borrar"),
