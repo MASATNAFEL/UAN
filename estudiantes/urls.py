2111a9fc05b8cf6f42a070c21e11047ff8d175a5
MASATNAFEL <103325439+MASATNAFEL@users.noreply.github.com>
2026-09-19 15:45:38 -0500

    cambios sin proposito
    cambios para visualizar diferentes enfoques

--- estudiantes/urls.py
+++ estudiantes/urls.py
@@ -5,11 +5,12 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("info/<str:cedula_ciudadania>/", views.informacion, name="informacion"),
-    path("car/<str:codcarpeta>/", views.informacion1, name="informacion1"),
+    path("car/<str:codcarpeta>/", views.inforcarpeta, name="infocarpeta"),
     path("info/<str:cedula_ciudadania>/edit", views.editar, name="editar"),
-    path("info/<str:cedula_ciudadania>/borrar", views.borrar, name="borrar"),
-    path("info/<str:codcarpeta>/borrar", views.borrar1, name="borrar1"),
+    path("car/<str:codcarpeta>/edit", views.modificar, name="modificar"),
+     
     path("buscar/", views.buscar, name="buscar"),
+    path("consultar/", views.consultar, name="consultar"),
     path("registro/", views.addestudiante, name="registro"),
     path("carpeta/", views.addcarpeta, name="carpeta"),
     
--- estudiantes/views.py
+++ estudiantes/views.py
@@ -6,15 +6,16 @@ from django.shortcuts import get_object_or_404,render,redirect
 
 def index(request):
     lista_Estudiantes = Estudiante.objects.filter(activo=True).order_by("nombre")
-    context = {"lista_Estudiantes":lista_Estudiantes}
+    lista_Carpetas = Carpeta.objects.filter(activo=True).order_by("codcarpeta")
+    context = {"lista_Estudiantes":lista_Estudiantes, "lista_Carpetas":lista_Carpetas}
     return render(request,"estudiantes/index.html",context)
     
 def informacion(request,cedula_ciudadania):
-    estudiante=get_object_or_404(Estudiante,pk=cedula_ciudadania)
-    return render(request,"estudiantes/informacion.html", {"estudiante":estudiante})
-def informacion1(request,codcarpeta):
-    carpeta=get_object_or_404(Carpeta,pk=codcarpeta)
-    return render(request,"estudiantes/informacion2.html", {"carpeta":carpeta})
+    listado=get_object_or_404(Estudiante,pk=cedula_ciudadania)
+    return render(request,"estudiantes/informacion.html", {"listado":listado, "url_borrar":"borrar"})
+def inforcarpeta(request,codcarpeta):
+    listado=get_object_or_404(Carpeta,pk=codcarpeta)
+    return render(request,"estudiantes/informacion.html", {"listado":listado, "url_borrar":"eliminar"})
 
 def editar(request,cedula_ciudadania):
     print(request.method)
@@ -29,6 +30,18 @@ def editar(request,cedula_ciudadania):
         form = FormularioEstudiante(instance=estudiante)
     return render(request, 'estudiantes/editar.html', {'form': form,'estudiante':estudiante})
 
+def modificar(request,codcarpeta):
+    print(request.method)
+    carpeta = get_object_or_404(Carpeta, pk=codcarpeta)
+
+    if request.method =='POST' :
+        form = FormularioCarpeta(request.POST, instance=carpeta)
+        if form.is_valid():
+            form.save()
+            return redirect('informacion',carpeta.pk)
+    else:
+        form = FormularioCarpeta(instance=carpeta)
+    return render(request, 'estudiantes/editar.html', {'form': form,'carpeta':carpeta})
 def buscar(request):
     consulta_nombre=request.GET.get("n","")
     consulta_cedula=request.GET.get("c","")
@@ -41,6 +54,16 @@ def buscar(request):
         resultado=resultado.filter(Q(cedula_ciudadania=consulta_cedula))
 
     context={"consulta_nombre":consulta_nombre,"consulta_cedula":consulta_cedula,"resultado":resultado}
+    return render(request,"estudiantes/buscar.html",context)
+
+def consultar(request):
+    consulta_codcarpeta=request.GET.get("a","")
+    resultado=Carpeta.objects.order_by("codcarpeta")
+
+    if consulta_codcarpeta:
+        resultado=resultado.filter(Q(codcarpeta__icontains=consulta_codcarpeta))
+
+    context={"consulta_codcarpeta":consulta_codcarpeta,"resultado":resultado}
     return render(request,"estudiantes/buscar.html",context)
 
 def addestudiante(request):
@@ -68,9 +91,47 @@ def borrar(request, cedula_ciudadania):
         estudiante.activo=False
         estudiante.save()
         return redirect("index")
-def borrar1(request, codcarpeta):
+def eliminar(request, codcarpeta):
     if request.method=="POST":
         carpeta=get_object_or_404(Carpeta,pk=codcarpeta)
         carpeta.activo=False
         carpeta.save()
         return redirect("index")
+def prueba(request):
+   filtros = [{
+            "name": "a",
+            "campo": "codcarpeta",
+            "label": "Código carpeta",
+        },
+    ]
+   columnas = [
+        ("codcarpeta", "Código carpeta"),
+        ("activo", "Activo"),
+    ]
+   for filtro in filtros:
+        valor = request.GET.get(filtro["name"], "")
+
+        filtro["value"] = valor
+
+        if valor:
+            resultado = resultado.filter(
+                **{
+                    f'{filtro["campo"]}__icontains': valor
+                }
+            )
+            context = {
+        "resultado": resultado,
+        "filtros": filtros,
+        "columnas": [
+            {
+                "campo": campo,
+                "label": label,
+            }
+            for campo, label in columnas
+        ],
+    }
+            return render(
+        request,
+        "estudiantes/consultar.html",
+        context
+    )
